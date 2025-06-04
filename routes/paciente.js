64d1bc5ec3aaf0a56ca48422757131e2144822b3
Gabriel Pereira Silva <gabrielps022002@gmail.com>
2025-06-03 23:15:06 -0400

    Co-authored-by: Leonardo Lima Andrade <LeonardoLAndrade@users.noreply.github.com> Co-authored-by: Juliany <kamilyjuliany@users.noreply.github.com>

--- routes/paciente.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pacienteController = require('../controllers/pacienteController');
-
-router.post('/', pacienteController.registrar);
-router.get('/listar',pacienteController.listar);
-module.exports = router;
